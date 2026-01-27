d878bff1980041a5a5e91ceda6c32e683d82a36e
shambhala-lab <73578727+shambhala-lab@users.noreply.github.com>
2026-01-27 16:31:49 +0700

    Add secrets.R
    Postgresql connection

---
+++ secrets.R
@@ -0,0 +1,9 @@
+# secrets.R
+# ไฟล์นี้เก็บความลับ ห้ามส่งให้ใคร!
+db_config <- list(
+  host = "postgresql-datainterests.alwaysdata.net",
+  dbname = "datainterests_happylotto",
+  user = "datainterests",
+  pass = "Adisai_480858",
+  port = 5432
+)
